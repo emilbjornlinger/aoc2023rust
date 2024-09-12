96667a293a7a85167cff5834b22445bcd9e5470c
Emil Björnlinger <15017341+emilbjornlinger@users.noreply.github.com>
2024-09-12 18:36:38 +0200

    Updated input handling for day1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello day 1");
+    let input = include_str!("../input/input1.txt");
+    let output = part1(input);
+    dbg!(output);
 }
+
+fn part1(input: &str) -> String {
+    print!("{input}");
+    "This is the output".to_string()
+}
+
